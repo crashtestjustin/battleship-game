961c980214ee149749de8be3dcdc60b215c5f388
Justin Elliott <thecrashtestjustin@gmail.com>
2023-05-19 08:20:32 -0700

    restarting the gameboardFactory

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,12 +1,9 @@
 import { ship } from "./ship.js";
 
-export const gameboard = () => {
-  const initializeBoard = () => {
-    let boardArray = [];
-    let squareArray = [];
-    for (let i = 0; i < 10; i++) {
-      let alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-      for (let j = 0; j < 10; j++) {}
-    }
+export const gameboardFactory = () => {
+  const gameboard = {
+    boardArray: [],
+    missedGuesses: [],
+    shipsSunk: 0,
   };
 };
