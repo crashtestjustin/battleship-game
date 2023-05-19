2c5b0109af54d080acd8745e41414b2daab1964b
Justin Elliott <thecrashtestjustin@gmail.com>
2023-05-19 07:56:49 -0700

    initiated gameboard function and test

---
+++ src/gameboard.js
@@ -0,0 +1,12 @@
+import { ship } from "./ship.js";
+
+export const gameboard = () => {
+  const initializeBoard = () => {
+    let boardArray = [];
+    let squareArray = [];
+    for (let i = 0; i < 10; i++) {
+      let alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+      for (let j = 0; j < 10; j++) {}
+    }
+  };
+};
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,80 +1,122 @@
 import { ship } from "../src/ship.js";
 
-test("Battleship Test", () => {
+test("Create Battleship Test", () => {
   const battleship = ship("Battleship");
   const result = {
+    name: battleship.name,
     length: battleship.length,
     hitCount: battleship.hitCount,
     sunk: battleship.sunk,
   };
-  expect(result).toEqual({ length: 4, hitCount: 0, sunk: false });
+  expect(result).toEqual({
+    name: "Battleship",
+    length: 4,
+    hitCount: 0,
+    sunk: false,
+  });
 });
 
-test("hit Battleship Test", () => {
+test("Hit Count Increment Battleship Test", () => {
   const battleship = ship("Battleship");
   battleship.hit();
   battleship.hit();
   const result = {
+    name: battleship.name,
     length: battleship.length,
     hitCount: battleship.hitCount,
     sunk: battleship.sunk,
   };
-  expect(result).toEqual({ length: 4, hitCount: 2, sunk: false });
+  expect(result).toEqual({
+    name: "Battleship",
+    length: 4,
+    hitCount: 2,
+    sunk: false,
+  });
 });
 
-test("Sink Battleship Test", () => {
+test("Battleship Sunk should be TRUE", () => {
   const battleship = ship("Battleship");
   battleship.hit();
   battleship.hit();
   battleship.hit();
   battleship.hit();
   const result = {
+    name: battleship.name,
     length: battleship.length,
     hitCount: battleship.hitCount,
     sunk: battleship.sunk,
   };
-  expect(result).toEqual({ length: 4, hitCount: 4, sunk: true });
+  expect(result).toEqual({
+    name: "Battleship",
+    length: 4,
+    hitCount: 4,
+    sunk: true,
+  });
 });
 
-test("Patrol Boat Test", () => {
+test("Generate Patrol Boat Test", () => {
   const boat = ship("Patrol Boat");
   const result = {
+    name: boat.name,
     length: boat.length,
     hitCount: boat.hitCount,
     sunk: boat.sunk,
   };
-  expect(result).toEqual({ length: 2, hitCount: 0, sunk: false });
+  expect(result).toEqual({
+    name: "Patrol Boat",
+    length: 2,
+    hitCount: 0,
+    sunk: false,
+  });
 });
 
-test("Submarine Test", () => {
+test("Generate Submarine Test", () => {
   const submarine = ship("Submarine");
   const result = {
+    name: submarine.name,
     length: submarine.length,
     hitCount: submarine.hitCount,
     sunk: submarine.sunk,
   };
-  expect(result).toEqual({ length: 3, hitCount: 0, sunk: false });
+  expect(result).toEqual({
+    name: "Submarine",
+    length: 3,
+    hitCount: 0,
+    sunk: false,
+  });
 });
 
-test("submarine sink Test", () => {
+test("Submarine Sink Test", () => {
   const submarine = ship("Submarine");
   submarine.hit();
   submarine.hit();
   submarine.hit();
   const result = {
+    name: submarine.name,
     length: submarine.length,
     hitCount: submarine.hitCount,
     sunk: submarine.sunk,
   };
-  expect(result).toEqual({ length: 3, hitCount: 3, sunk: true });
+  expect(result).toEqual({
+    name: "Submarine",
+    length: 3,
+    hitCount: 3,
+    sunk: true,
+  });
 });
 
-test("Carrier Test", () => {
+test("Generate Carrier Test", () => {
   const carrier = ship("Carrier");
   const result = {
+    name: carrier.name,
     length: carrier.length,
     hitCount: carrier.hitCount,
     sunk: carrier.sunk,
   };
-  expect(result).toEqual({ length: 5, hitCount: 0, sunk: false });
+  expect(result).toEqual({
+    name: "Carrier",
+    length: 5,
+    hitCount: 0,
+    sunk: false,
+  });
 });
