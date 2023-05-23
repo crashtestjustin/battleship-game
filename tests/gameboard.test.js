09f1cbb47a521195c5b78e108ce9f4ff9eec9b0e
Justin Elliott <thecrashtestjustin@gmail.com>
2023-05-22 21:32:15 -0700

    ran successful tests for hit and miss ship attacks

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -11,3 +11,38 @@ test("gameboard factory randomly places 5 ships to boardArray", () => {
   newBoard.placeShipsRandom();
   expect(newBoard.boardArray.length).toEqual(5);
 });
+
+const mockShip = {
+  hitCount: 0,
+  hit: function () {
+    this.hitCount++;
+  },
+  location: [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ],
+};
+
+test("hit a ship", () => {
+  const newBoard = gameboardFactory();
+  newBoard.placeShipsRandom();
+  const shipHit = newBoard.receiveAttack(newBoard.boardArray[0][0]);
+  if (shipHit !== null) {
+    expect(shipHit.hitCount).toBe(1);
+  } else {
+    expect(newBoard.missedGuesses).toContainEqual(newBoard.boardArray[0][0]);
+  }
+});
+
+test("miss a ship", () => {
+  const newBoard = gameboardFactory();
+  newBoard.placeShipsRandom();
+  const shipHit = newBoard.receiveAttack([0.5, 1]);
+  if (shipHit !== null) {
+    expect(shipHit.hitCount).toBe(1);
+  } else {
+    expect(newBoard.missedGuesses).toContainEqual([0.5, 1]);
+    console.log("MISS");
+  }
+});
