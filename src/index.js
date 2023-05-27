f4d8038dea70e03824230fad9ae9d4f975edae5c
Justin Elliott <thecrashtestjustin@gmail.com>
2023-05-27 08:35:29 -0700

    linking main webpack entry to dom content file

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { gameloop } from "./gameloop";
+import { header, body, footer } from "./domstuff";
+
+function application() {
+  const app = document.querySelector(".application");
+
+  const headerDiv = header();
+  app.appendChild(headerDiv);
+
+  const bodyDiv = body();
+  app.appendChild(bodyDiv);
+
+  const footerDiv = footer();
+  app.appendChild(footerDiv);
+
+  return app;
+}
+
+application();
