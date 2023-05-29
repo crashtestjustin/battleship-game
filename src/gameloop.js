cc374b1e9863bf7462579b5515794f0b162bda20
Justin Elliott <thecrashtestjustin@gmail.com>
2023-05-29 07:56:15 -0700

    adding notes for future changes
    adding notes for future changes and disabling empty
    while loop

--- src/gameloop.js
+++ src/gameloop.js
@@ -7,9 +7,13 @@ export function newGameLoop() {
     const cpuPlayer = playerFactory();
     userPlayer.oponentGameboard.placeShipsRandom();
     cpuPlayer.oponentGameboard.placeShipsRandom();
-    while (
-      userPlayer.oponentGameboard.shipsSunk < 5 ||
-      cpuPlayer.oponentGameboard.shipsSunk < 5
-    ) {}
+    // while (
+    //   userPlayer.oponentGameboard.shipsSunk < 5 ||
+    //   cpuPlayer.oponentGameboard.shipsSunk < 5
+    // ) {}
   });
 }
+
+//using while loop to run through plaer guess and cpu random guess
+// need to associate each grid-square with a coordinate somehow??
+//once one of the board's shipsSunk is 5 => run a determination for a winner
